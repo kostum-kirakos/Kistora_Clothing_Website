75f7a033dc0fec37ece4b4bd6e2e72970bbfab22
LKOUSTIM MOHAMED <132625229+kostum-kirakos@users.noreply.github.com>
2024-07-04 10:10:39 +0100

    adding the Circle Button to the home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,13 @@ homeSwiper.on("slideChangeTransitionEnd", () => {
     const prevSlide = homeSwiper.slides[homeSwiper.previousIndex];
     prevSlide.classList.remove("reveal");
 });
+
+/* Circle Btn */
+
+let circleText = circleBtn.querySelector(".circle-text");
+circleText.innerHTML = circleText.textContent
+    .split("")
+    .map((char, index) => `<span style="transform: rotate(${index * 28.3}deg)">${char}</span>`)
+    .join("");
+
+
